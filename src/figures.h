3029ba6f9565aa85a47fb59964ea63b65d23006c
Artar1337 <armastra@gmail.com>
2019-03-31 19:05:02 +0700

    Updated figures.h file

---
+++ src/figures.h
@@ -0,0 +1,14 @@
+#ifndef FIG_GUARD
+#define FIG_GUARD
+int circle(float x, float y, float r);
+int triangle(float x1, float y1, float x2, float y2, float x3, float y3);
+int polygone(
+        float x1,
+        float y1,
+        float x2,
+        float y2,
+        float x3,
+        float y3,
+        float x4,
+        float y4);
+#endif
