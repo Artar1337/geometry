2fb62ea5e02023d476d6203ece8696e7c15654be
Artar1337 <armastra@gmail.com>
2019-05-05 01:25:05 +0700

    Changed figures.h

--- src/figures.h
+++ src/figures.h
@@ -1,8 +1,30 @@
 #ifndef FIG_GUARD
 #define FIG_GUARD
-int circle(float x, float y, float r);
+void circle(float x, float y, float r);
+float circleP(float r);
+float circleS(float r);
 int triangle(float x1, float y1, float x2, float y2, float x3, float y3);
+float triangleP(float x1, float y1, float x2, float y2, float x3, float y3);
+float triangleS(float x1, float y1, float x2, float y2, float x3, float y3);
 int polygone(
+        float x1,
+        float y1,
+        float x2,
+        float y2,
+        float x3,
+        float y3,
+        float x4,
+        float y4);
+float polygoneP(
+        float x1,
+        float y1,
+        float x2,
+        float y2,
+        float x3,
+        float y3,
+        float x4,
+        float y4);
+float polygoneS(
         float x1,
         float y1,
         float x2,
